ff2127d237ca6a93189c33e6f32641daa4b5ba7b
Elya-Zam <elyazssousa@gmail.com>
2025-12-09 09:06:24 -0400

    patch e put

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { buscaContatoPorId, buscarTodosContatos } from "./servico/buscaServico.j
 import { deletaContato } from "./servico/deletaServico.js";
 import { validacontato } from "./validacao/valida.js";
 import { cadastraContato } from "./servico/cadastroServico.js";
+import { atualizaContato, atualizaContatoParcial } from "./servico/atualizaServico.js";
 
 const app = new express();
 app.use(express.json())
@@ -19,6 +20,46 @@ app.post('/contatos', async (req, res) => {
         res.status(204).end();
     } else {
         res.status(400).json({mensagem: contatoValido.mensagem})
+    }
+})
+
+app.put('/contatos/:id', async (req, res) =>{
+    const {id} = req.params; //Desestruturação: extrai as propiedades diretamente e as atribui a variaveis do mesmo nome;
+
+    const {nome, telefone, email} = req.body; //Desestruturação
+    const contatoValido = validacontato(nome, telefone, email)
+
+    if (!contatoValido) {
+        res.status(400).json({'mensagem': 'Algum valor não está informado'})
+    } else {
+        const resultado = await atualizaContato(id, nome, telefone, email)
+        if (resultado.affectedRows > 0) {
+            res.status(202).json({'mensagem': 'Parabens, atualizado'})
+        } else {
+            res.status(404).json({'mensagem': 'Registro não encontrado'})
+        }
+    }
+})
+
+app.patch('/contatos/:id', async (req, res) => {
+    const {id} = req.params;
+    const {nome, telefone, email} = req.body;
+
+    const camposAtualizar = {};
+    if(nome) camposAtualizar.nome = nome;
+    if(telefone) camposAtualizar.telefone = telefone;
+    if(email) camposAtualizar.email = email;
+
+    if (Object.keys(camposAtualizar).length === 0 ) {
+        return res.status(400).json({'mensagem': 'Algum campo inválido'})
+    } else {
+        const resultado = await atualizaContatoParcial(id, camposAtualizar)
+
+        if (resultado.affectedRows > 0) {
+            res.status(202).json({'mensagem': 'Parabens, atualizado'})
+        } else {
+            res.status(404).json({'mensagem': 'Registro não encontrado'})
+        }
     }
 })
 
